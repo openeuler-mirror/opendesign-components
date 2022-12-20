ff520549aad4e9042669c98ec45d5e7fa1b7c09a
Bang <15622356989@163.com>
2022-12-20 17:20:55 +0800

    feat: 新增Switch

---
+++ packages/opendesign/src/components/switch/__demo__/SwitchShape.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { OSWitch, SwitchShapeT } from '../index';
+const switchVal1 = ref(false);
+const switchVal2 = ref(true);
+</script>
+
+<template>
+  <section>
+    <OSWitch v-model="switchVal1" :shape="SwitchShapeT.NORMAL" />
+    <OSWitch v-model="switchVal2" :shape="SwitchShapeT.ROUND" />
+  </section>
+</template>
---
+++ packages/opendesign/src/components/switch/__demo__/SwitchSize.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { OSWitch, SwitchShapeT, SwitchSizeT } from '../index';
+const switchVal1 = ref(false);
+const switchVal2 = ref(true);
+</script>
+
+<template>
+  <section>
+    <OSWitch v-model="switchVal1" :shape="SwitchShapeT.NORMAL" :size="SwitchSizeT.SMALL" />
+    <OSWitch v-model="switchVal1" :shape="SwitchShapeT.NORMAL" :size="SwitchSizeT.NORMAL" />
+    <OSWitch v-model="switchVal1" :shape="SwitchShapeT.NORMAL" :size="SwitchSizeT.LARGE" />
+    <OSWitch v-model="switchVal2" :shape="SwitchShapeT.ROUND" :size="SwitchSizeT.SMALL" />
+    <OSWitch v-model="switchVal2" :shape="SwitchShapeT.ROUND" :size="SwitchSizeT.NORMAL" />
+    <OSWitch v-model="switchVal2" :shape="SwitchShapeT.ROUND" :size="SwitchSizeT.LARGE" />
+  </section>
+</template>
