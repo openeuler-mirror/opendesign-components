af29468efd0a8449b2c59fcfdca046c9445d2a55
devin <caowendong@huawei.com>
2023-02-02 15:56:08 +0800

    增加前缀

--- packages/opendesign/src/components/intersection-observer/__demo__/IntersectionDemo.vue
+++ packages/opendesign/src/components/intersection-observer/__demo__/IntersectionDemo.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { reactive, ref } from 'vue';
-import { IntersectionObserver } from '../index';
+import { OIntersectionObserver } from '../index';
 import { OButton } from '../../button';
 import '../../button/style';
 
@@ -27,14 +27,14 @@ const toggle = (key: 'visible1' | 'visible2') => {
 
   <div class="tip" @click="toggle('visible2')">enter into view: {{ content }}</div>
   <div class="wrap">
-    <IntersectionObserver @intersection="(isEnter:boolean, en) => onIntersection(isEnter, en)">
+    <OIntersectionObserver @intersection="(isEnter:boolean, en) => onIntersection(isEnter, en)">
       <div v-if="rlt.visible1" class="content" :name="content">first row</div>
       <p v-else class="content">changed: first row</p>
       <div @click="toggle('visible1')">toggle</div>
-    </IntersectionObserver>
-    <IntersectionObserver @intersection="(isEnter:boolean, en) => onIntersection(isEnter, en)">
+    </OIntersectionObserver>
+    <OIntersectionObserver @intersection="(isEnter:boolean, en) => onIntersection(isEnter, en)">
       <OButton v-if="rlt.visible2" @click="toggle('visible2')"> toggle btn</OButton>
-    </IntersectionObserver>
+    </OIntersectionObserver>
   </div>
 </template>
 <style lang="scss" scoped>
