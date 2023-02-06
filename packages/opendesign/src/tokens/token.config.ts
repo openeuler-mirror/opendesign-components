4d00c7efa582f55e793335de3c0bc6dd1d859e2f
devin <caowendong@huawei.com>
2023-02-06 17:34:20 +0800

    update token

--- packages/opendesign/src/tokens/token.config.ts
+++ packages/opendesign/src/tokens/token.config.ts
@@ -3,6 +3,6 @@ module.exports = {
   output: '../components/style/',
   themes: ['light', 'dark'],
   defaultTheme: 'light',
-  tokenFile: ['./color.json', './size.json'],
+  tokenFile: ['./tokens.json'],
   codeSnippetsFile: '../../../../.vscode/opendesign.token.code-snippets'
 };
