b4d3839e696a89ec1bd99301cab9fe550447c90f
devin <caowendong@huawei.com>
2023-02-19 16:04:05 +0800

    增加form

---
+++ packages/opendesign/src/components/form/OForm.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import { ref, computed, watch } from 'vue';
+</script>
+<template>
+  <div class="o-form"></div>
+</template>
---
+++ packages/opendesign/src/components/form/__demo__/FormBasic.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="page-demo"></div>
+</template>
+<style lang="scss"></style>
