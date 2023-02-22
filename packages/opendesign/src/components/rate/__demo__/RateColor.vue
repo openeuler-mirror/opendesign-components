182f750b4cc96a52884269616d1cb70ba43eda89
Bang <15622356989@163.com>
2023-02-22 23:46:21 +0800

    feat: 新增rate评分组件

---
+++ packages/opendesign/src/components/rate/__demo__/RateColor.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { ORate } from '../index';
+
+const rateVal = ref(3);
+</script>
+
+<template>
+  <h4>不同颜色</h4>
+  <section>
+    <ORate v-model="rateVal" />
+    <ORate v-model="rateVal" color="primary" />
+    <ORate v-model="rateVal" color="warning" />
+    <ORate v-model="rateVal" color="danger" />
+  </section>
+</template>
+
+<style scoped>
+section {
+  flex-direction: column;
+  align-items: flex-start;
+}
+</style>
