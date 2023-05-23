111f69037a3e1dd4bedafc8961d14a6c0fd1a39c
devin <caowendong@huawei.com>
2023-05-23 09:49:42 +0800

    size 更新

--- packages/portal-ak/src/pages/button/TheIndex.vue
+++ packages/portal-ak/src/pages/button/TheIndex.vue
@@ -5,7 +5,7 @@ import { OButton, OLink, OIcon, OIconRefresh, OIconAdd, OIconChevronRight } from
   <div class="page-demo">
     <h3>类型 & 尺寸</h3>
     <section>
-      L:
+      M:
       <OButton>运营按钮</OButton>
       <OButton variant="solid" color="primary">强调按钮</OButton>
       <OButton variant="solid" color="primary" :icon="OIconAdd">强调按钮</OButton>
@@ -20,12 +20,12 @@ import { OButton, OLink, OIcon, OIconRefresh, OIconAdd, OIconChevronRight } from
       <OIcon :icon="OIconRefresh" button />
     </section>
     <section>
-      M:
-      <OButton>运营按钮</OButton>
-      <OButton variant="solid" color="primary">强调按钮</OButton>
-      <OButton variant="solid" color="primary" :icon="OIconAdd">强调按钮</OButton>
-      <OButton variant="outline" color="primary">描边按钮</OButton>
-      <OButton variant="outline" color="primary" :icon="OIconAdd">描边按钮</OButton>
+      L:
+      <OButton size="large">运营按钮</OButton>
+      <OButton variant="solid" color="primary" size="large">强调按钮</OButton>
+      <OButton variant="solid" color="primary" :icon="OIconAdd" size="large">强调按钮</OButton>
+      <OButton variant="outline" color="primary" size="large">描边按钮</OButton>
+      <OButton variant="outline" color="primary" :icon="OIconAdd" size="large">描边按钮</OButton>
       <OLink>文本按钮</OLink>
       <OLink :icon="OIconAdd">文本按钮</OLink>
       <OLink>
