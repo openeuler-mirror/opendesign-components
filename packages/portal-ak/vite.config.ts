b718ca91f2cf304747cd3975217c352eb652eb28
devin <caowendong@huawei.com>
2023-05-29 18:24:36 +0800

    ak 引用mixin

--- packages/portal-ak/vite.config.ts
+++ packages/portal-ak/vite.config.ts
@@ -15,6 +15,13 @@ export default defineConfig({
       '@assets': path.resolve(__dirname, './src/assets'),
     },
   },
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: "@use '@/assets/theme/mixin.scss' as *;",
+  //     },
+  //   },
+  // },
   server: {
     port: 3202,
   },
