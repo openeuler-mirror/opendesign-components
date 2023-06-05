914bcfb493fd4ccac80bc2ac40d7892c1422f21e
devin <caowendong@huawei.com>
2023-06-05 20:56:59 +0800

    add button

---
+++ packages/portal-ak/src/pages/TheText.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import '@components/style';
+const list = ['display1', 'display2', 'display3', 'display4', 'h1', 'h2', 'h3', 'text1', 'text2', 'tip1', 'tip2'];
+const title = 'Text';
+</script>
+<template>
+  <div class="page-demo">
+    <h1>{{ title }}</h1>
+    <div v-for="t in list" :key="t" :class="[`o-txt-${t}`]">{{ t }}</div>
+  </div>
+</template>
+<style lang="scss">
+.the-home {
+  color: #333;
+}
+</style>
