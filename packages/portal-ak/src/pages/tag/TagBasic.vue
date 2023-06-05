68f9cd34e25f739aee6df13499702a7d9a0796a3
Bang <15622356989@163.com>
2023-06-05 18:05:45 +0800

    fix: 优化tag demo

--- packages/portal-ak/src/pages/tag/TagBasic.vue
+++ packages/portal-ak/src/pages/tag/TagBasic.vue
@@ -5,24 +5,24 @@ import { OTag } from '@components/index';
 <template>
   <h3>运营标签</h3>
   <section>
-    <OTag color="primary">NEW</OTag>
-    <OTag color="primary">HOT</OTag>
-    <OTag color="primary">推荐</OTag>
-    <OTag color="primary" class="tag-ascend">优质模型</OTag>
+    <OTag color="primary" round="pill">NEW</OTag>
+    <OTag color="primary" round="pill">HOT</OTag>
+    <OTag color="primary" round="pill">推荐</OTag>
+    <OTag round="pill" class="tag-ascend">优质模型</OTag>
   </section>
 
   <h3>信息标签</h3>
   <section>
-    <OTag>Python</OTag>
-    <OTag>C++</OTag>
-    <OTag class="tag-normal-inverse">Python</OTag>
+    <OTag round="pill">Python</OTag>
+    <OTag round="pill">C++</OTag>
+    <OTag round="pill" class="tag-normal-inverse">Python</OTag>
   </section>
 
   <h3>状态标签</h3>
   <section>
-    <OTag color="success">已完成</OTag>
-    <OTag class="tag-unstart">未开始</OTag>
-    <OTag color="danger">未通过</OTag>
+    <OTag round="pill" color="success">已完成</OTag>
+    <OTag round="pill" class="tag-unstart">未开始</OTag>
+    <OTag round="pill" color="danger">未通过</OTag>
   </section>
 </template>
 <style lang="scss"></style>
--- packages/portal-ak/src/pages/tag/TagClosable.vue
+++ packages/portal-ak/src/pages/tag/TagClosable.vue
@@ -8,8 +8,8 @@ const handleClose = () => {
 <template>
   <h3>可删除标签</h3>
   <section>
-    <OTag closable @close="handleClose">可操作标签1</OTag>
-    <OTag closable @close="handleClose">可操作标签2</OTag>
-    <OTag closable @close="handleClose">可操作标签3</OTag>
+    <OTag round="pill" closable @close="handleClose">可操作标签1</OTag>
+    <OTag round="pill" closable @close="handleClose">可操作标签2</OTag>
+    <OTag round="pill" closable @close="handleClose">可操作标签3</OTag>
   </section>
 </template>
