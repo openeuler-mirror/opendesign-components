7ee667033eecae1927ec33cb987462d30c6c5984
devin <caowendong@huawei.com>
2023-06-06 11:59:26 +0800

    按钮优化

--- packages/portal-ak/src/pages/button/BtnBasic.vue
+++ packages/portal-ak/src/pages/button/BtnBasic.vue
@@ -28,11 +28,11 @@ import { OButton, OLink, OIcon, OIconRefresh, OIconAdd, OIconChevronRight } from
       <OButton round="pill" variant="outline" color="primary" :icon="OIconAdd" size="large">描边按钮</OButton>
       <OLink color="primary" size="large">文本按钮</OLink>
       <OLink color="primary" :icon="OIconAdd" size="large">文本按钮</OLink>
-      <OLink color="primary" class="c-link-kunpeng">
-        鲲鹏文本按钮 <template #suffix><OIconChevronRight class="o-link-arrow" /></template>
+      <OLink color="primary" class="c-link-kunpeng" hover-underline>
+        鲲鹏文本按钮 <template #suffix><OIconChevronRight class="c-link-kunpeng-arrow" /></template>
       </OLink>
       <OLink color="primary" class="c-link-ascend">
-        昇腾文本按钮 <template #suffix><OIconChevronRight class="o-link-arrow" /></template>
+        昇腾文本按钮 <template #suffix><div class="c-link-ascend-arrow"></div></template>
       </OLink>
       <OLink hover-underline size="large">链接按钮</OLink>
       <OIcon :icon="OIconRefresh" button size="large" />
@@ -47,10 +47,10 @@ import { OButton, OLink, OIcon, OIconRefresh, OIconAdd, OIconChevronRight } from
       <OLink disabled color="primary">文本按钮</OLink>
       <OLink disabled color="primary" :icon="OIconAdd">文本按钮</OLink>
       <OLink disabled color="primary" class="c-link-kunpeng">
-        鲲鹏文本按钮 <template #suffix><OIconChevronRight class="o-link-arrow" /></template>
+        鲲鹏文本按钮 <template #suffix><OIconChevronRight /></template>
       </OLink>
       <OLink disabled color="primary" class="c-link-ascend">
-        昇腾文本按钮 <template #suffix><OIconChevronRight class="o-link-arrow" /></template>
+        昇腾文本按钮 <template #suffix><div class="c-link-ascend-arrow"></div></template>
       </OLink>
       <OLink disabled hover-underline>链接按钮</OLink>
       <OIcon disabled :icon="OIconRefresh" button />
