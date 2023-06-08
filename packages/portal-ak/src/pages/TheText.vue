bc049f4b55ce31a629859034759319a0aaf89d20
devin <caowendong@huawei.com>
2023-06-08 11:08:55 +0800

    up text

--- packages/portal-ak/src/pages/TheText.vue
+++ packages/portal-ak/src/pages/TheText.vue
@@ -1,16 +1,28 @@
 <script setup lang="ts">
 import '@components/style';
-const list = ['display1', 'display2', 'display3', 'display4', 'h1', 'h2', 'h3', 'text1', 'text2', 'tip1', 'tip2'];
-const title = 'Text';
+const list = ['display1', 'display2', 'display3', 'h1', 'h2', 'h3', 'h4', 'text1', 'text2', 'tip1', 'tip2'];
+const letter = 'abcdefghijklmnopqrstuvwxyz';
+const letterUp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const number = '0123456789';
 </script>
 <template>
   <div class="page-demo">
-    <h1>{{ title }}</h1>
-    <div v-for="t in list" :key="t" :class="[`o-txt-${t}`]">{{ t }}</div>
+    <div v-for="t in list" :key="t" :class="[`o-txt-${t}`]">
+      <div class="row">
+        <div class="name">[{{ t }}]</div>
+        <div>
+          <div>{{ number }}</div>
+          <div>{{ letter }}</div>
+          <div>{{ letterUp }}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <style lang="scss">
-.the-home {
-  color: #333;
+.name {
+  font-size: 18px;
+  line-height: 24px;
+  margin: 16px 0 4px;
 }
 </style>
