a0f955561dfbdd76daee61c1844f64a6100b83ec
devin <caowendong@huawei.com>
2023-06-08 10:33:30 +0800

    增加深色示例

--- packages/portal-ak/src/pages/button/BtnBasic.vue
+++ packages/portal-ak/src/pages/button/BtnBasic.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { OButton, OLink, OIcon, OIconRefresh, OIconAdd, OIconChevronRight } from '@components/index';
+
+import { darkTheme } from '@/shared/theme';
 </script>
 <template>
   <h3>类型 & 尺寸</h3>
@@ -57,5 +59,45 @@ import { OButton, OLink, OIcon, OIconRefresh, OIconAdd, OIconChevronRight } from
     <OLink disabled hover-underline>链接按钮</OLink>
     <OIcon disabled :icon="OIconRefresh" button />
   </section>
+
+  <h3>深色</h3>
+  <div :data-o-theme="darkTheme">
+    <section>
+      M:
+      <OButton round="pill" class="c-btn-ascend" variant="solid" color="primary">运营按钮</OButton>
+      <OButton round="pill" variant="solid" color="primary">强调按钮</OButton>
+      <OButton round="pill" variant="solid" color="primary" :icon="OIconAdd">强调按钮</OButton>
+      <OButton round="pill" variant="outline" color="primary">描边按钮</OButton>
+      <OButton round="pill" variant="outline" color="primary" :icon="OIconAdd">描边按钮</OButton>
+      <OLink color="primary">文本按钮</OLink>
+      <OLink color="primary" :icon="OIconAdd">文本按钮</OLink>
+      <OLink color="primary" class="c-link-kunpeng" hover-underline>
+        文本按钮 <template #suffix><OIconChevronRight class="c-link-kunpeng-arrow" /></template>
+      </OLink>
+      <OLink color="primary" class="c-link-ascend">
+        昇腾文本按钮 <template #suffix><div class="c-link-ascend-arrow"></div></template>
+      </OLink>
+      <OLink hover-underline>链接按钮</OLink>
+      <OIcon :icon="OIconRefresh" button />
+    </section>
+    <section>
+      禁用:
+      <OButton round="pill" disabled class="c-btn-ascend" variant="solid" color="primary">运营按钮</OButton>
+      <OButton round="pill" disabled variant="solid" color="primary">强调按钮</OButton>
+      <OButton round="pill" disabled variant="solid" color="primary" :icon="OIconAdd">强调按钮</OButton>
+      <OButton round="pill" disabled variant="outline" color="primary">描边按钮</OButton>
+      <OButton round="pill" disabled variant="outline" color="primary" :icon="OIconAdd">描边按钮</OButton>
+      <OLink disabled color="primary">文本按钮</OLink>
+      <OLink disabled color="primary" :icon="OIconAdd">文本按钮</OLink>
+      <OLink disabled color="primary" class="c-link-kunpeng">
+        鲲鹏文本按钮 <template #suffix><OIconChevronRight /></template>
+      </OLink>
+      <OLink disabled color="primary" class="c-link-ascend">
+        昇腾文本按钮 <template #suffix><div class="c-link-ascend-arrow"></div></template>
+      </OLink>
+      <OLink disabled hover-underline>链接按钮</OLink>
+      <OIcon disabled :icon="OIconRefresh" button />
+    </section>
+  </div>
 </template>
 <style lang="scss"></style>
--- packages/portal-ak/src/shared/theme.ts
+++ packages/portal-ak/src/shared/theme.ts
@@ -1,3 +1,12 @@
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 const theme = ref('');
+
+const darkTheme = computed(() => {
+  return theme.value.replace('light', 'dark');
+});
+const lightTheme = computed(() => {
+  return theme.value.replace('dark', 'light');
+});
+
 export default theme;
+export { darkTheme, lightTheme };
