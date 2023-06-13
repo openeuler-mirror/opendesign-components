682d44fcd3660cdaa16fa3100ae7b73a90487af3
devin <caowendong@huawei.com>
2023-06-13 15:29:56 +0800

    解决scrollintoview的bug

--- packages/portal-ak/src/pages/tab/TheBasic.vue
+++ packages/portal-ak/src/pages/tab/TheBasic.vue
@@ -6,6 +6,10 @@ const tabList = [
   { id: 2, label: '页签文字2', icon: OIconAdd },
   { id: 3, label: '页签文字3', icon: OIconCalendar },
   { id: 4, label: '页签文字4', icon: OIconAdd },
+  { id: 5, label: '页签文字5', icon: OIconCalendar },
+  { id: 6, label: '页签文字6', icon: OIconAdd },
+  { id: 7, label: '页签文字7', icon: OIconCalendar },
+  { id: 8, label: '页签文字8', icon: OIconAdd },
 ];
 </script>
 <template>
@@ -36,6 +40,19 @@ const tabList = [
   </OTab>
   <br />
   <OTab variant="text" :line="false" class="c-tab-large">
+    <OTabPane v-for="item in tabList" :key="item.id" class="pane" :value="item.id">
+      <template #nav>
+        <div class="o-tab-nav-title">
+          <OIcon :icon="item.icon" />
+          {{ item.label }}
+        </div>
+      </template>
+
+      <div style="height: 100px; background-color: #fff">pane {{ item.id }}</div>
+    </OTabPane>
+  </OTab>
+  <br />
+  <OTab variant="text" :line="false" class="c-tab-large" :model-value="4">
     <OTabPane v-for="item in tabList" :key="item.id" class="pane" :value="item.id">
       <template #nav>
         <div class="o-tab-nav-title">
