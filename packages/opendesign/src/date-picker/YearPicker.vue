c2884533f60010c8214fac0bed14e94bed6c1a65
devin <caowendong@huawei.com>
2023-07-18 20:10:33 +0800

    year 支持滚动选择

--- packages/opendesign/src/date-picker/YearPicker.vue
+++ packages/opendesign/src/date-picker/YearPicker.vue
@@ -1,23 +1,29 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { nextTick, onMounted, ref, watch } from 'vue';
 import { OScroller } from '../scroller';
-import { OIcon } from '../icon';
-import { IconCalendarPrevYear, IconCalendarNextYear } from '../_utils/icons';
 import { chunk } from '../_utils/helper';
+import { PickerDate } from './picker-date';
+import { isFunction } from '../_utils/is';
+import { scrollCellToView } from './date';
 
 interface CellT {
-  value: number;
+  years: number;
   label: string;
   isNow: boolean;
-  outView: boolean;
+  disabled: boolean;
 }
 
 const props = withDefaults(
   defineProps<{
-    value: number;
+    value: PickerDate;
+    displayYears?: (currentYear: number) => Array<{ value: number; label: string }>;
+    column?: number;
+    disableDate: (current: Date, type?: 'start' | 'end') => boolean;
   }>(),
   {
     value: undefined,
+    column: 3,
+    displayYears: undefined,
   }
 );
 
@@ -26,94 +32,106 @@ const emits = defineEmits<{
   (e: 'select', value: number): void;
 }>();
 
-const selectValue = ref<number>(props.value);
+const selectValue = ref<number>(props.value.years);
 const yearRangeLabel = ref<string>();
 const yearList = ref<CellT[][]>([]);
-const viewYear = ref<number>(0);
 
-const DECADE_VALUE = 10;
-const updateViewYears = (year?: number) => {
-  const nowYear = new Date().getFullYear();
-  const y = year ?? nowYear;
-  const sy = y - (y % DECADE_VALUE);
-  const ey = sy + DECADE_VALUE - 1;
+const hScrollRef = ref<InstanceType<typeof OScroller>>();
+const scrollIntoView = (smooth?: boolean) => {
+  nextTick(() => {
+    scrollCellToView(hScrollRef.value, '.o-picker-cell-selected', {
+      smooth,
+      align: 'center',
+    });
+  });
+};
 
-  const list = [];
-  for (let i = -1; i < 11; i++) {
-    const v = i + sy;
+const getYearList = (year?: number) => {
+  const nowYear = year ?? new Date().getFullYear();
+  const start = nowYear - (nowYear % 100) - 100;
+
+  const list: Array<{ value: number; label: string }> = [];
+  for (let i = 0; i < 201; i++) {
     list.push({
-      label: `${v}`,
-      value: v,
-      outView: v > ey || v < sy,
-      isNow: v === nowYear,
+      value: i + start,
+      label: `${i + start}`,
     });
   }
-  console.log(selectValue.value);
+  return list;
+};
 
-  viewYear.value = sy;
-  yearList.value = chunk(list, 3);
+const updateViewYears = (year: number) => {
+  const nowYear = new Date().getFullYear();
+
+  const yl = isFunction(props.displayYears) ? props.displayYears(year) : getYearList(year);
+
+  const list = yl.map((item) => {
+    return {
+      label: item.label,
+      value: item,
+      isNow: item.value === nowYear,
+      disabled: props.disableDate(new Date(item.value, 0)),
+    };
+  });
+
+  yearList.value = chunk(list, props.column);
   yearRangeLabel.value = `${list[1].label} - ${list[10].value}`;
 };
 
-updateViewYears(props.value);
+updateViewYears(props.value.years);
 
 watch(
   () => props.value,
-  (v: number) => {
-    selectValue.value = v;
-    updateViewYears(v);
+  (v: PickerDate) => {
+    if (selectValue.value !== v.years) {
+      selectValue.value = v.years;
+      scrollIntoView(true);
+    }
   }
 );
 
-/**
- * head 操作
- */
-const headBtnClick = (actionType: 'sub' | 'add') => {
-  const sign = actionType === 'add' ? 1 : -1;
-  updateViewYears(viewYear.value + sign * 10);
-};
-
 const selectCell = (cell: CellT) => {
-  selectValue.value = cell.value;
-  updateViewYears(cell.value);
+  if (cell.disabled || cell.years === selectValue.value) {
+    return;
+  }
+
+  selectValue.value = cell.years;
 
   emits('select', selectValue.value);
   emits('update:value', selectValue.value);
 };
+
+onMounted(() => {
+  scrollIntoView();
+});
 </script>
 <template>
-  <div class="o-picker-year">
-    <div class="o-picker-head">
-      <div class="o-picker-head-btns">
-        <OIcon class="o-picker-btn" button :icon="IconCalendarPrevYear" @click="headBtnClick('sub')" />
-      </div>
-      <div class="o-picker-head-value">
-        <slot name="year-head-label" :select-value="selectValue" :view-year-list="yearList">{{ yearRangeLabel }}</slot>
-      </div>
-      <div class="o-picker-head-btns">
-        <OIcon class="o-picker-btn" button :icon="IconCalendarNextYear" @click="headBtnClick('add')" />
-      </div>
-    </div>
-    <div class="o-picker-main">
-      <OScroller ref="hScrollRef" size="small">
-        <div v-for="(row, idx) in yearList" :key="idx" class="o-picker-row">
-          <div
-            v-for="item in row"
-            :key="item.value"
-            class="o-picker-cell o-py-cell"
-            :class="{
-              'o-picker-cell-selected': selectValue === item.value,
-              'o-picker-cell-out-view': item.outView,
-              'o-picker-cell-now': item.isNow,
-            }"
-            @click="(e) => selectCell(item)"
-          >
-            <div class="o-picker-cell-val">
-              <slot name="cell-year" v-bind="item">{{ item.label }}</slot>
-            </div>
-          </div>
+  <OScroller ref="hScrollRef" size="small" class="o-picker-year">
+    <div class="more-top"></div>
+    <div
+      v-for="(row, idx) in yearList"
+      :key="idx"
+      class="o-picker-row"
+      :style="{
+        '--column': props.column,
+      }"
+    >
+      <div
+        v-for="item in row"
+        :key="item.years"
+        class="o-picker-cell o-py-cell"
+        :class="{
+          'o-picker-cell-selected': selectValue === item.years,
+          'o-picker-cell-now': item.isNow,
+          'o-picker-cell-disabled': item.disabled,
+        }"
+        @click="(e) => selectCell(item)"
+      >
+        <div class="o-picker-cell-val">
+          <slot name="cell-year" v-bind="item">{{ item.label }}</slot>
         </div>
-      </OScroller>
+      </div>
     </div>
-  </div>
+    <div class="more-bottom"></div>
+  </OScroller>
 </template>
