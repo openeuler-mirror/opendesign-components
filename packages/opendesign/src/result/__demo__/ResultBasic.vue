f9a0ae2f8c2d6f077b4566d9ed160115e266cdaa
Bang <15622356989@163.com>
2023-09-06 17:04:47 +0800

    result组件支持image插槽&样式适配

--- packages/opendesign/src/result/__demo__/ResultBasic.vue
+++ packages/opendesign/src/result/__demo__/ResultBasic.vue
@@ -1,31 +1,44 @@
 <script setup lang="ts">
 import { OButton } from '../../button';
 import '../../button/style';
+import { ODivider } from '../../divider';
+import '../../divider/style';
 import { OResult } from '../index';
 </script>
 
 <template>
   <h4>基础用法</h4>
   <section>
-    <OResult title="操作错误" description="请修改配置项" status="info" style="flex: 1" />
-    <OResult title="操作错误" description="请修改配置项" status="success" style="flex: 1">
+    <OResult title="操作错误" description="请修改配置项" status="info" />
+    <ODivider />
+    <OResult title="操作错误" description="请修改配置项" status="success">
       <template #extra>
         <OButton>返回</OButton>
       </template>
     </OResult>
-    <OResult title="操作错误" description="请修改配置项" status="warning" style="flex: 1">
+    <ODivider />
+    <OResult title="操作错误" description="请修改配置项" status="warning">
       <template #extra>
         <OButton>返回</OButton>
       </template>
     </OResult>
-    <OResult title="操作错误" description="请修改配置项" status="danger" style="flex: 1">
+    <ODivider />
+    <OResult title="操作错误" description="请修改配置项" status="danger">
       <template #extra>
         <OButton>返回</OButton>
       </template>
     </OResult>
-
-    <OResult title="操作错误" description="请修改配置项" style="flex: 1" />
-    <OResult title="操作错误" style="flex: 1" />
-    <OResult description="请修改配置项" style="flex: 1" />
+    <ODivider />
+    <OResult title="操作错误" description="请修改配置项" />
+    <ODivider />
+    <OResult title="操作错误" />
+    <ODivider />
+    <OResult description="请修改配置项" />
   </section>
 </template>
+
+<style lang="scss" scoped>
+section {
+  flex-direction: column;
+}
+</style>
