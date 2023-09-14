11bd6eabd32684f54f69667121a9aceaecf2ed28
devin <caowendong@huawei.com>
2023-09-14 19:22:08 +0800

    修复选项容器最小宽度

--- packages/opendesign/src/pagination/__demo__/PaginationBasic.vue
+++ packages/opendesign/src/pagination/__demo__/PaginationBasic.vue
@@ -2,10 +2,7 @@
 import { ref } from 'vue';
 import { OPagination } from '../index';
 const currentPage = ref(5);
-const total = ref(0);
-setTimeout(() => {
-  total.value = 400;
-}, 1000);
+const total = ref(400);
 
 const pageSize = 8;
 const pageSizes = [8, 16, 32, 64];
@@ -23,7 +20,7 @@ const onChange = (value: { page: number; pageSize: number }) => {
         <br />
         <OPagination :total="total" :page="currentPage" :page-size="pageSize" :page-sizes="pageSizes" round="pill" @change="onChange" />
         <br />
-        <!-- <OPagination :total="total" :page="currentPage" :page-size="pageSize" :page-sizes="pageSizes" round="12px" @change="onChange" />
+        <OPagination :total="total" :page="currentPage" :page-size="pageSize" :page-sizes="pageSizes" round="12px" @change="onChange" />
       </div>
     </div>
     <div>
@@ -34,20 +31,19 @@ const onChange = (value: { page: number; pageSize: number }) => {
         <OPagination :total="total" round="pill" :page="currentPage" :page-size="pageSize" :page-sizes="pageSizes" variant="solid" @change="onChange" />
         <br />
         <OPagination :total="total" round="12px" :page="currentPage" :page-size="pageSize" :page-sizes="pageSizes" variant="solid" @change="onChange" />
-      </div> -->
       </div>
     </div>
-    <h4>Simple</h4>
-    <section>
-      <div>当前页：{{ currentPage }}</div>
-      <div class="row">
-        <OPagination v-model:page="currentPage" :total="100" simple @change="onChange" />
-        <br />
-        <OPagination v-model:page="currentPage" variant="solid" :total="100" simple @change="onChange" />
-        <br />
-        <OPagination v-model:page="currentPage" variant="solid" :total="100" simple round="pill" @change="onChange" />
-      </div>
-    </section>
   </div>
+  <h4>Simple</h4>
+  <section>
+    <div>当前页：{{ currentPage }}</div>
+    <div class="row">
+      <OPagination v-model:page="currentPage" :total="100" simple @change="onChange" />
+      <br />
+      <OPagination v-model:page="currentPage" variant="solid" :total="100" simple @change="onChange" />
+      <br />
+      <OPagination v-model:page="currentPage" variant="solid" :total="100" simple round="pill" @change="onChange" />
+    </div>
+  </section>
 </template>
 <style lang="scss"></style>
