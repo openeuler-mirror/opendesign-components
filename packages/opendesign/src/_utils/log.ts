ad281109f111ae3453cd655834ec8b6224f68fe8
devin <caowendong@huawei.com>
2023-12-13 15:43:08 +0800

    feat: log增加namespace

--- packages/opendesign/src/_utils/log.ts
+++ packages/opendesign/src/_utils/log.ts
@@ -14,15 +14,31 @@ function printLog(level: LogLevel, ...msg: any[]) {
 }
 
 export class Log {
+  prefix: string;
+  constructor(namespace?: string) {
+    this.prefix = '';
+    if (namespace) {
+      this.prefix = `[${namespace}]`;
+    }
+  }
   info(...msg: any[]) {
+    if (this.prefix) {
+      msg.unshift(this.prefix);
+    }
     printLog('info', ...msg);
   }
 
   warn(...msg: any[]) {
+    if (this.prefix) {
+      msg.unshift(this.prefix);
+    }
     printLog('warn', ...msg);
   }
 
   error(...msg: any[]) {
+    if (this.prefix) {
+      msg.unshift(this.prefix);
+    }
     printLog('error', ...msg);
   }
 }
