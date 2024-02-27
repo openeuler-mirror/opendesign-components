e867ace7603bf55c3eac8010bee5a937d8ff3394
devin <caowendong@huawei.com>
2024-02-27 15:02:44 +0800

    feat(vUid): 参数增加随机id

--- packages/opendesign/src/directves/uid.ts
+++ packages/opendesign/src/directves/uid.ts
@@ -3,12 +3,16 @@ import { uniqueId } from '../_utils/helper';
 import { isFunction } from '../_utils/is';
 
 const getUId = uniqueId;
-
+/**
+ * 为元素生成id，如果元素已设置id，则使用已有id（解决ssr时会导致Hydration attribute mismatch）
+ * 1. 默认会直接生成随机id
+ * 2. 支持传递函数，自定义处理
+ */
 const vUid: ObjectDirective = {
   created(el: HTMLElement, binding) {
     const setIdFn = binding.value;
     if (isFunction(setIdFn)) {
-      setIdFn(el);
+      setIdFn(el, uniqueId());
     } else {
       el.setAttribute('id', el.id || uniqueId());
     }
