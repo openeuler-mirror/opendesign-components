37e5f9792e4e5a11d1d7724535db99843437edad
devin <caowendong@huawei.com>
2024-05-30 20:46:34 +0800

    feat(pagination): 支持total插槽

---
+++ packages/opendesign/src/pagination/__demo__/PaginationSlot.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { OPagination } from '../index';
+const currentPage = ref(5);
+const total = ref(5000);
+
+const pageSize = 8;
+const pageSizes = [8, 16, 32, 64];
+const onChange = (value: { page: number; pageSize: number }) => {
+  console.log(value);
+};
+</script>
+<template>
+  <h4>Slot</h4>
+  <div>
+    <div>
+      <div>total</div>
+      <div>
+        <OPagination :total="total" :page="currentPage" show-total :page-size="pageSize" :page-sizes="pageSizes" round="12px" @change="onChange">
+          <template #total> total: -- {{ total }} --</template>
+        </OPagination>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss"></style>
