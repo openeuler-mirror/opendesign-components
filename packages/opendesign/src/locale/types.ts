4113cd81fd55f9955f591b99cb7f9eaa9544975c
devin <caowendong@huawei.com>
2024-06-25 15:31:51 +0800

    doc: 增加国际化设计文档

--- packages/opendesign/src/locale/types.ts
+++ packages/opendesign/src/locale/types.ts
@@ -1,10 +1,30 @@
 export interface LanguageT {
   locale: string;
-  pagination: {
+  common?: {
+    empty: string;
+    loading: string;
+  };
+  pagination?: {
     goto: string;
     page: string;
     countPerPage: string;
     total: string;
+  };
+  upload?: {
+    buttonLabel: string;
+    drag: string;
+    dragHover: string;
+    retry: string;
+    delete: string;
+    preview: string;
+    edit: string;
+  };
+  select?: {
+    cancel: string;
+    confirm: string;
+  };
+  input?: {
+    limit: string;
   };
 }
 export type i18nLanguages = Record<string, LanguageT>;
