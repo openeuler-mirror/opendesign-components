9ea1446754e6e0e9dc091cee3e913365360a5beb
devin <caowendong@huawei.com>
2024-11-21 21:17:56 +0800

    refactor: 重构createObserverInstance支持传入options，优化useResizeObserver，两者都添加destroy方法

--- packages/opendesign/src/hooks/use-intersection-observer.ts
+++ packages/opendesign/src/hooks/use-intersection-observer.ts
@@ -1,108 +1,125 @@
 import { isFunction } from '../_utils/is';
 
-export type IntersectionListenerT = (entry: IntersectionObserverEntry) => void;
-
-/**
- * 监听池
- * isFirst: 监听的第一次回调
- */
-const observerPool = new WeakMap<
+type ElementPool = WeakMap<
   HTMLElement,
   {
     element: HTMLElement;
-    callbacks: Array<IntersectionListenerT>;
+    callbacks: Array<(entry: IntersectionObserverEntry, isIntersecting: boolean) => void>;
   } | null
->();
+>;
+interface ObserveInstance {
+  observer: IntersectionObserver;
+  record: number;
+  elementPool: ElementPool;
+}
+const defaultKey = {};
+/**
+ * 实例池
+ */
+const instancePool = new WeakMap<IntersectionObserverInit | typeof defaultKey, ObserveInstance>();
 
-// 记录监听数量
-let record = 0;
+function createObserverInstance(options?: IntersectionObserverInit): ObserveInstance {
+  let instance = instancePool.get(options || defaultKey);
+  if (!instance) {
+    const observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        const ele = entry.target as HTMLElement;
+        const ins = instance?.elementPool.get(ele);
 
-// 创建监听实例
-let instance: ResizeObserver | null = null;
+        if (!ins) {
+          return;
+        }
 
-function createObserver() {
-  return new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
-    entries.forEach((entry) => {
-      const ele = entry.target as HTMLElement;
-      const ins = observerPool.get(ele);
+        ins?.callbacks?.forEach((fn) => fn(entry, entry.isIntersecting));
+      });
+    }, options);
 
-      if (!ins) {
-        return;
-      }
-
-      ins?.callbacks?.forEach((fn) => fn(entry));
-    });
-  });
+    /**
+     * 监听元素池
+     */
+    const elementPool: ElementPool = new WeakMap();
+    instance = {
+      observer,
+      record: 0,
+      elementPool: elementPool,
+    };
+    instancePool.set(options || defaultKey, instance);
+  }
+  return instance;
 }
 
 /**
- * 监听元素尺寸变化，
- * ele: 监听元素；
- * onResize: resize回调（entry: 尺寸变化元素，isFirst: 是否为初次监听时的回调);
+ * 监听元素进入视口；
  */
-export function useIntersectionObserver() {
+export function useIntersectionObserver(options?: IntersectionObserverInit) {
+  // 创建监听实例
+  const instance = createObserverInstance(options);
+
   return {
     /**
      * 监听实例
      */
-    observer: instance,
+    observer: instance.observer,
     /**
      * 创建监听实例
-     * ele: 监听元素
-     * listener: resize回调, 移除监听时需要指定该监听函数
-     * isFirst: 是否为初次监听时的回调
+     * el: 添加监听的元素
+     * listener: 进入视口回调, 移除监听时需要指定该监听函数
      */
-    observe: (el?: HTMLElement, listener?: IntersectionListenerT) => {
+    observe: (el: HTMLElement, listener: (entry: IntersectionObserverEntry) => void) => {
       if (!el || !isFunction(listener)) {
         return null;
       }
 
-      // 同一元素，只创建一次ResizeObserver，通过数组存储回调
-      const val = observerPool.get(el);
+      // 同一元素，只创建一次Observer，通过数组存储回调
+      const val = instance.elementPool.get(el);
       if (val) {
         val.callbacks.push(listener);
       } else {
-        if (!instance) {
-          instance = createObserver();
-        }
-        instance.observe(el);
-        record++;
+        instance.observer.observe(el);
+        instance.record++;
 
-        observerPool.set(el, {
+        instance.elementPool.set(el, {
           element: el,
           callbacks: [listener],
         });
       }
 
-      return instance;
+      return instance.observer;
     },
     /**
-     * 移除监听
+     * 移除对某元素的监听
+     * el: 要移除监听的元素
      * listener: 要移除的监听函数，如果不传，则使用初始化时的回调
      */
-    unobserve: (el?: HTMLElement, listener?: IntersectionListenerT) => {
+    unobserve: (el: HTMLElement, listener: (entry: IntersectionObserverEntry) => void) => {
       if (!el || !isFunction(listener) || !instance) {
         return;
       }
 
-      const val = observerPool.get(el);
+      const val = instance.elementPool.get(el);
 
       if (val) {
         const idx = val.callbacks.indexOf(listener);
         val.callbacks.splice(idx, 1);
-        if (val.callbacks.length === 0) {
-          // 当el无监听时，销毁resizeObserver
-          instance.unobserve(el);
-          observerPool.delete(el);
 
-          // 无监听元素，就断开
-          record--;
-          if (record === 0) {
-            instance.disconnect();
-            instance = null;
+        // 当el无监听回调时，不再监听该元素
+        if (val.callbacks.length === 0) {
+          instance.observer.unobserve(el);
+          instance.elementPool.delete(el);
+          instance.record--;
+
+          // 当无需要监听的元素，就断开
+          if (instance.record === 0) {
+            instance.observer.disconnect();
           }
         }
       }
+    },
+    /**
+     * 销毁观察器
+     */
+    destroy() {
+      instance.observer.disconnect();
     },
   };
 }
