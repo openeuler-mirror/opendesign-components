563c8b9334a3d6feb7829677a1dea1aca7f4166d
AC-0308 <pzzxtyh@qq.com>
2025-07-22 17:08:31 +0800

    doc: 📝 增加表格(table)的文档

---
+++ packages/opendesign/src/table/__docs__/__case__/TableDiyHead.vue
@@ -0,0 +1,53 @@
+<docs lang="md">
+<!--zh-CN-->
+### 自定义表头
+
+<!--en-US-->
+### Costume Header
+</docs>
+
+<script setup lang="ts">
+import { OTable } from '@opensig/opendesign';
+import { getTableData } from './data';
+
+interface ColItemT {
+  label: string;
+  key: string;
+  rowspan?: number;
+  colspan?: number;
+  last?: boolean;
+}
+const columns1: ColItemT[][] = [
+  [
+    { label: 'Name', key: 'name', rowspan: 2 },
+    { label: 'Info', key: 'info', colspan: 3 },
+    { label: 'Other', key: 'other', rowspan: 2, last: true },
+  ],
+  [
+    { label: 'Salary', key: 'salary' },
+    { label: 'Address', key: 'address' },
+    { label: 'Email', key: 'email' },
+  ],
+];
+const columnKeys = ['name', 'salary', 'address', 'email', 'other'];
+
+const tableData = getTableData(6);
+</script>
+<template>
+  <div class="sec">
+    <OTable :data="tableData" :columns="columnKeys">
+      <template #header>
+        <tr v-for="(row, idx) in columns1" :key="idx">
+          <th v-for="col in row" :key="col.key" :rowspan="col.rowspan" :colspan="col.colspan" :class="{ last: col.last }">
+            {{ col.label }}
+          </th>
+        </tr>
+      </template>
+    </OTable>
+  </div>
+</template>
+<style lang="scss">
+.sec {
+  margin-bottom: 24px;
+}
+</style>
---
+++ packages/opendesign/src/table/__docs__/index.zh-CN.md
@@ -0,0 +1,21 @@
+---
+sidebar: OTable 数据表格
+---
+
+# 数据表格
+
+## 使用
+
+<!-- @usage tableUsage -->
+
+## 示例
+
+<!-- @case TableBasic -->
+<!-- @case TableDiy -->
+<!-- @case TableDiyHead -->
+<!-- @case TablePagination -->
+<!-- @case TableSpan -->
+
+## Api
+
+<!-- @api OTable -->
