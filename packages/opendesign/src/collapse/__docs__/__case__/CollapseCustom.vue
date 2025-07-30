a3b9adf424e569dca2e0d53f5323cd9ec483da15
sakurayinfei <970412446@qq.com>
2025-07-30 14:53:40 +0800

    feat[doc]: 新增collapse文档

---
+++ packages/opendesign/src/collapse/__docs__/__case__/CollapseCustom.vue
@@ -0,0 +1,49 @@
+<docs lang="md">
+<!-- zh-CN -->
+
+### 自定义标题
+
+通过 `title` 插槽可以自定义标题
+
+<!-- en-US -->
+
+### Custom Title
+
+You can customize the title through the `title` slot
+</docs>
+<script setup lang="ts">
+import { OCollapse, OCollapseItem, OIconFile } from '@opensig/opendesign';
+</script>
+
+<template>
+  <OCollapse accordion>
+    <OCollapseItem :value="1">
+      <template #title>
+        <OIconFile class="icon" />
+        <span>title 1</span>
+      </template>
+      <div class="content1"></div>
+    </OCollapseItem>
+    <OCollapseItem title="title 2" :value="2">
+      <template #title>
+        <OIconFile class="icon" />
+        <span>title 2</span>
+      </template>
+      <div class="content2"></div>
+    </OCollapseItem>
+  </OCollapse>
+</template>
+<style lang="scss" scoped>
+.icon {
+  font-size: var(--o-icon_size-m);
+  margin-right: var(--o-gap-2);
+}
+.content1 {
+  background-color: yellowgreen;
+  height: 150px;
+}
+.content2 {
+  background-color: lightblue;
+  height: 200px;
+}
+</style>
