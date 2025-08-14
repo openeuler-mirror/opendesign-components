3e40c0c32d6ec30ff7c157d3492ac6a1c39aa16e
AC-0308 <pzzxtyh@qq.com>
2025-08-14 20:25:51 +0800

    feat(menu): ✨ 菜单增加small尺寸

--- packages/opendesign/src/menu/OMenuItem.vue
+++ packages/opendesign/src/menu/OMenuItem.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { computed, inject } from 'vue';
+import { computed, inject, ref } from 'vue';
 import { menuItemProps } from './types';
 import { menuInjectKey, subMenuInjectKey } from './provide';
 import { isUndefined } from '../_utils/is';
+import { isOverflown } from '../_utils/dom.ts';
 
 const props = defineProps(menuItemProps);
 
@@ -12,6 +13,8 @@ const emits = defineEmits<{
 
 const menuInjection = inject(menuInjectKey, null);
 const subMenuInjection = inject(subMenuInjectKey, null);
+
+const { size = ref('medium'), showTooltip, hideTooltip } = menuInjection || {};
 
 const isSelected = computed(() => {
   if (menuInjection) {
@@ -38,30 +41,39 @@ const onItemClick = (ev: MouseEvent) => {
 
 const depth = subMenuInjection ? subMenuInjection.depth + 1 : 1;
 
-const style = computed(() => {
-  if (depth === 1) {
-    return {};
-  } else {
-    return {
-      paddingLeft: `${(menuInjection?.levelIndent.value ?? 20) * depth}px`,
-    };
-  }
+
+const cssVars = computed(() => {
+  return {
+    '--menu-item-level': depth,
+  };
 });
 
 menuInjection?.menuTree.addChild({
   value: props.value as string,
   parentVal: subMenuInjection?.value,
 });
+
+const itemContentRef = ref<HTMLSpanElement>();
+const handleMouseenter = (e: MouseEvent) => {
+  if (!e.target || !isOverflown(itemContentRef.value)) {
+    return;
+  }
+  showTooltip?.({ el: e.target as HTMLElement, content: itemContentRef.value?.innerText });
+};
+const handleMouseleave = () => {
+  hideTooltip?.();
+};
 </script>
 
 <template>
-  <li class="o-menu-item" :class="{ 'o-menu-item-selected': isSelected, 'o-menu-item-disabled': $props.disabled }" :style="style" @click="onItemClick">
-    <span v-if="props.icon || $slots.icon" class="o-menu-item-icon">
+  <li class="o-menu-item" :class="{ 'o-menu-item-selected': isSelected, 'o-menu-item-disabled': $props.disabled }"
+    :style="cssVars" @click="onItemClick" @mouseenter="handleMouseenter" @mouseleave="handleMouseleave">
+    <span v-if="size !== 'small' && (props.icon || $slots.icon)" class="o-menu-item-icon">
       <slot name="icon">
         <component :is="props.icon" />
       </slot>
     </span>
-    <span class="o-menu-item-content">
+    <span class="o-menu-item-content" ref="itemContentRef">
       <slot></slot>
     </span>
   </li>
