30808e5e495ea737ce29deea40c9f227501657b8
sakurayinfei <970412446@qq.com>
2025-09-01 17:51:40 +0800

    fix(doc): 优化DocConfigProvide组件provide数据的逻辑

--- packages/docs/src/components/DocConfigProvide.vue
+++ packages/docs/src/components/DocConfigProvide.vue
@@ -1,8 +1,32 @@
 <script setup lang="ts">
-import { provide, useAttrs } from 'vue';
+import { provide, useAttrs, inject } from 'vue';
+import { hyphenate } from '@/utils/named';
+
 defineOptions({
   inheritAttrs: false,
 });
-provide('docs-config', useAttrs());
+const parentConfig = inject<Record<string, any>>('docs-config', {});
+const selfConfig = useAttrs();
+const newConfig = new Proxy(selfConfig, {
+  get(target, key: string) {
+    if (key in target) {
+      return target[key];
+    }
+    if (key in parentConfig) {
+      return parentConfig[key];
+    }
+    const hKey = hyphenate(key);
+    if (hKey in target) {
+      return target[hKey];
+    }
+    if (hKey in parentConfig) {
+      return parentConfig[hKey];
+    }
+  },
+  has(target, p) {
+    return p in target || p in parentConfig;
+  },
+});
+provide('docs-config', newConfig);
 </script>
 <template><slot></slot></template>
