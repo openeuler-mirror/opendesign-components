828d7cb1ec9b2730d2c718f882748060978e06a9
sakurayinfei <970412446@qq.com>
2025-09-04 20:06:27 +0800

    fix: 修复皮肤样式重构后未修改导入路径的bug

--- packages/docs/src/assets/style/index.ts
+++ packages/docs/src/assets/style/index.ts
@@ -1,4 +1,4 @@
-import '@opensig/opendesign/theme/common.scss';
+import '@opensig/opendesign/theme/grid.scss';
 import './font-family.scss';
 import './var-map.scss';
 import './style.scss';
--- packages/portal/src/main.ts
+++ packages/portal/src/main.ts
@@ -2,12 +2,12 @@ import { createApp } from 'vue';
 import './assets/style/style.scss';
 import 'normalize.css';
 
-import '@opendesign-src/theme/opendesign/light.token.css';
-import '@opendesign-src/theme/opendesign/dark.token.css';
-import '@opendesign-src/theme/ascend/a.dark.token.css';
-import '@opendesign-src/theme/ascend/a.light.token.css';
-import '@opendesign-src/theme/kunpeng/k.dark.token.css';
-import '@opendesign-src/theme/kunpeng/k.light.token.css';
+import '@opendesign-src/theme/opendesign/common/light.token.css';
+import '@opendesign-src/theme/opendesign/common/dark.token.css';
+import '@opendesign-src/theme/ascend/common/a.dark.token.css';
+import '@opendesign-src/theme/ascend/common/a.light.token.css';
+import '@opendesign-src/theme/kunpeng/common/k.dark.token.css';
+import '@opendesign-src/theme/kunpeng/common/k.light.token.css';
 import './assets/style/media.token.scss';
 
 import { router } from '@/router';
