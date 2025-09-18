5cb334931fc4f3492e8839b27d91b49464b1b405
菜得让人发毛 <2328204591@qq.com>
2025-09-18 09:44:33 +0800

    feat(doc): 新增OResult组件文档

---
+++ packages/opendesign/src/result/__docs__/__case__/ResultNormal.vue
@@ -0,0 +1,37 @@
+<docs lang="md">
+<!-- zh-CN -->
+
+#### 结果态
+
+<!-- en-US -->
+
+#### Result state
+</docs>
+<script setup lang="ts">
+import { OResult, OButton } from '@opensig/opendesign';
+
+const title = '提交成功';
+const description = '内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容';
+const status = 'success';
+</script>
+<template>
+  <div class="demo-result-wrap">
+    <OResult :title="title" :description="description" :status="status">
+      <template #extra>
+        <OButton class="extra-btn" size="large" variant="outline" color="primary" round="pill">返回首页</OButton>
+        <OButton class="extra-btn" size="large" variant="outline" color="primary" round="pill">我的计划</OButton>
+      </template>
+    </OResult>
+  </div>
+</template>
+<style lang="scss">
+@use '../../../_styles/mixin.scss';
+.demo-result-wrap {
+  .extra-btn {
+    margin: 0 8px;
+    @include respond-to('<=pad') {
+      margin: 0 6px;
+    }
+  }
+}
+</style>
---
+++ packages/opendesign/src/result/__docs__/index.en-US.md
@@ -0,0 +1,16 @@
+---
+sidebar: OResult
+kind: feedback
+---
+
+# Result display
+
+## 示例
+
+<!-- @usage ResultUsage -->
+<!-- @case:a|k|e ResultNormal -->
+<!-- @case:a|k|e ResultAbnormal -->
+
+## Api
+
+<!-- @api OResult -->
