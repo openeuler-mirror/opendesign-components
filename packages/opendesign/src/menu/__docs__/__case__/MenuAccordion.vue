a587c33d57da79d85a1a6867e660416501ea3954
sakurayinfei <970412446@qq.com>
2025-10-15 15:24:40 +0800

    doc(menu): 新增文档

---
+++ packages/opendesign/src/menu/__docs__/__case__/MenuAccordion.vue
@@ -0,0 +1,33 @@
+<docs lang="md">
+<!-- zh-CN -->
+
+### 手风琴
+
+<!-- en-US -->
+
+### Accordion
+</docs>
+<script setup lang="ts">
+import { OMenu, OSubMenu, OMenuItem } from '@opensig/opendesign';
+</script>
+<template>
+  <OMenu accordion>
+    <OSubMenu value="1">
+      <template #title>Sub menu 1</template>
+      <OMenuItem value="1-1">Item 1-1</OMenuItem>
+      <OMenuItem value="1-2">Item 1-2</OMenuItem>
+    </OSubMenu>
+    <OSubMenu value="2">
+      <template #title>Sub menu 2</template>
+      <OMenuItem value="2-1">Item 2-1</OMenuItem>
+      <OSubMenu value="2-2">
+        <template #title>Sub menu 2-2</template>
+        <OMenuItem value="2-2-1" disabled>Item 2-2-1</OMenuItem>
+      </OSubMenu>
+      <OSubMenu value="2-3">
+        <template #title>Sub menu 2-3</template>
+        <OMenuItem value="2-3-1">Item 2-3-1</OMenuItem>
+      </OSubMenu>
+    </OSubMenu>
+  </OMenu>
+</template>
