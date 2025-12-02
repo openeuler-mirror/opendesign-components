ae91dc78d020d1131cd65a84fcd09d4b8e24cb0c
AC-0308 <pzzxtyh@qq.com>
2025-12-02 19:40:02 +0800

    refactor(OAnchor): 💥 【Breaking Change】重构OAnchor
    - 增加尺寸区别: small、medium、menu
    - 增加一级锚点圆圈指示器
    - 增加行溢出隐藏加气泡提示
    - 增加外部链跳转逻辑(除_self都认为是外链)
    - 增加item的disabled属性
    - 去除hover与active的背托、增加hover与active的字体颜色
    - 修改粗体字号为600

--- packages/opendesign/src/anchor/__demo__/AnchorBasic.vue
+++ packages/opendesign/src/anchor/__demo__/AnchorBasic.vue
@@ -10,17 +10,46 @@ import { OAnchor, OAnchorItem } from '../index';
       <div id="block2" class="block">block2</div>
       <div id="block3" class="block">block3</div>
       <div id="block3-1" class="block">block3-1</div>
+      <div id="block3-2" class="block">block3-2</div>
       <div id="block4" class="block">block4</div>
     </div>
     <div class="demo-anchor">
-      <OAnchor>
-        <OAnchorItem href="#block1" title="锚点1" />
-        <OAnchorItem href="#block2" title="锚点2" />
-        <OAnchorItem href="#block3" title="锚点3">
-          <OAnchorItem href="#block3-1" title="锚点3-1" />
-        </OAnchorItem>
-        <OAnchorItem href="#block4" title="锚点4" />
-      </OAnchor>
+      <div>
+        <div>medium:</div>
+        <OAnchor style="max-width: 200px" size="medium">
+          <OAnchorItem href="#block1" title="锚点1" />
+          <OAnchorItem href="#block2" title="bbbbbbbbbbbbbbbbbbbbbbbb" />
+          <OAnchorItem href="#block3" title="锚点3">
+            <OAnchorItem href="#block3-1" title="锚点3-1aaaaaaaaaaaaaaaaaaaa" />
+            <OAnchorItem href="#block3-2" title="锚点2" disabled/>
+          </OAnchorItem>
+          <OAnchorItem href="#block4" title="锚点4" />
+        </OAnchor>
+      </div>
+      <div>
+        <div>small:</div>
+        <OAnchor style="max-width: 200px" size="small">
+          <OAnchorItem href="#block1" title="锚点1" />
+          <OAnchorItem href="#block2" title="bbbbbbbbbbbbbbbbbbbbbbbb" />
+          <OAnchorItem href="#block3" title="锚点3">
+            <OAnchorItem href="#block3-1" title="锚点3-1aaaaaaaaaaaaaaaaaaaa" />
+            <OAnchorItem href="#block3-2" title="锚点2" />
+          </OAnchorItem>
+          <OAnchorItem href="#block4" title="锚点4" />
+        </OAnchor>
+      </div>
+      <div>
+        <div>menu:</div>
+        <OAnchor style="max-width: 200px" size="menu">
+          <OAnchorItem href="#block1" title="锚点1" />
+          <OAnchorItem href="#block2" title="锚点2" />
+          <OAnchorItem href="#block3" title="锚点3">
+            <OAnchorItem href="#block3-1" title="锚点3-1换行-openEuler Developer Day 2023 （简称 ODD 2023）是开放原子开源基金会旗下 openEuler 社区" />
+            <OAnchorItem href="#block3-2" title="锚点3-2" />
+          </OAnchorItem>
+          <OAnchorItem href="#block4" title="锚点4" />
+        </OAnchor>
+      </div>
     </div>
   </section>
 </template>
@@ -51,5 +80,6 @@ import { OAnchor, OAnchorItem } from '../index';
   position: fixed;
   top: 200px;
   right: 60px;
+  display: flex;
 }
 </style>
--- packages/opendesign/src/anchor/provide.ts
+++ packages/opendesign/src/anchor/provide.ts
@@ -3,8 +3,13 @@ import { InjectionKey, Ref } from 'vue';
 export const anchorInjectKey: InjectionKey<{
   addLink: (link: string) => void;
   removeLink: (link: string) => void;
-  handleClick: (ev: MouseEvent, link?: string) => void;
+  /**
+   * @deprecated 兼容旧版本，计划1.2.0移除
+   */
+  onItemClick: (options: { event: MouseEvent; link?: string }) => void;
   activeLink: Ref<string>;
+  scrollIntoView: (link: string) => void;
+  getChangeHash: () => boolean;
 }> = Symbol('provide-anchor');
 
 export const anchorItemInjectKey: InjectionKey<{
