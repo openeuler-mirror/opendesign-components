9c9dbc44bdb2273793049add5df07b5334051dbf
weixin_71507447 <2328204591@qq.com>
2025-12-23 16:59:02 +0800

    doc(rate): 刷新rate文档

--- packages/opendesign/src/rate/__docs__/__case__/ThemeRate.vue
+++ packages/opendesign/src/rate/__docs__/__case__/ThemeRate.vue
@@ -8,11 +8,7 @@
 - 尺寸为 `L`：`size` 属性 设置为 `large`
 - 颜色为红色：`color` 属性设置为 `primary`
 
-纯展示星星，使用 `OIconStar` 组件：
-
-- 尺寸为 `M`：类选择器 `c-star-full` 或 `c-star-empty` 中具有该尺寸的样式声明
-- 无评分颜色为灰色：设置类名 `c-star-empty`
-- 有评分颜色为红色：设置类名 `c-star-full`
+纯展示星星，尺寸使用 `medium`，添加属性 `readonly`，通过属性 `default-value` 控制星星是实星还是空星。
 
 <!-- en-US -->
 
@@ -23,19 +19,21 @@ For interactive stars, use the `ORate` component:
 - Size `L`: Set the `size` prop to `large`
 - Color red: Set the `color` prop to `primary`
 
-For display-only stars, use the `OIconStar` component:
-
-- Size `M`: The class selectors `c-star-full` or `c-star-empty` contain the corresponding style declarations for this size
-- No rating value (gray color): Apply the class name `c-star-empty`
-- With rating value (red color): Apply the class name `c-star-full`
+Purely display the stars, use `medium` for the size, add the attribute `readonly`, and control whether the star is a real star or an empty star through the attribute `default-value`.
 </docs>
 <script setup lang="ts">
-import { ORate, OIconStar } from '@opensig/opendesign';
+import { ORate } from '@opensig/opendesign';
 const labels = ['非常不满意', '不满意', ' 一般', '满意', '非常满意'];
 </script>
 <template>
-  <div class="box"><OIconStar class="c-star-full only-show" />4.0</div>
-  <div class="box"><OIconStar class="c-star-empty only-show" />暂无评分</div>
+  <div class="box">
+    <ORate :default-value="1" :count="1" color="primary" size="medium" readonly />
+    <span class="result">4.0</span>
+  </div>
+  <div class="box">
+    <ORate :count="1" color="primary" size="medium" readonly />
+    <span class="result">暂无评分</span>
+  </div>
   <div class="box operator">
     <ORate :default-value="1" :count="5" color="primary" size="large" />
   </div>
@@ -47,7 +45,7 @@ const labels = ['非常不满意', '不满意', ' 一般', '满意', '非常满�
 .box + .box {
   margin-top: 12px;
 }
-.only-show {
-  margin-right: 8px;
+.result {
+  margin-left: 8px;
 }
 </style>
