70ff8e7ab4335fe938302b061ef0118729bfb67b
weixin_71507447 <2328204591@qq.com>
2026-02-10 15:57:49 +0800

    doc(input): 刷新筛选输入框demo

--- packages/opendesign/src/input/__docs__/__case__/InputFilter.vue
+++ packages/opendesign/src/input/__docs__/__case__/InputFilter.vue
@@ -46,60 +46,84 @@ const options1 = [
 </script>
 <template>
   <div class="demo-input-filter-wrap">
-    <OInput class="demo-input-right" v-model="inputVal" size="large" placeholder="请输入手机号">
-      <template #prefix>
-        <OSelect v-model="selectedVal" size="large" class="demo-select" @click.stop>
-          <OOption v-for="(item, idx) in options" :label="item.label" :value="item.value" :key="idx"></OOption>
-        </OSelect>
-      </template>
-    </OInput>
+    <div class="demo-wrap-top">
+      <OInput class="demo-input" v-model="inputVal" size="large" placeholder="请输入手机号" />
 
-    <OInput class="demo-input-left" v-model="inputVal" size="large" placeholder="请输入手机号">
-      <template #suffix>
-        <OSelect v-model="selectedVal1" size="large" class="demo-select" @click.stop>
-          <OOption v-for="(item, idx) in options1" :label="item.label" :value="item.value" :key="idx"></OOption>
-        </OSelect>
-      </template>
-    </OInput>
+      <OSelect v-model="selectedVal" size="large" class="demo-select" @click.stop>
+        <OOption v-for="(item, idx) in options" :label="item.label" :value="item.value" :key="idx"></OOption>
+      </OSelect>
+    </div>
+
+    <div class="demo-wrap-bottom">
+      <OSelect v-model="selectedVal1" size="large" class="demo-select" @click.stop>
+        <OOption v-for="(item, idx) in options1" :label="item.label" :value="item.value" :key="idx"></OOption>
+      </OSelect>
+
+      <OInput class="demo-input" v-model="inputVal" size="large" placeholder="请输入手机号" />
+    </div>
   </div>
 </template>
 <style scoped lang="scss">
 .demo-input-filter-wrap {
-  display: flex;
-  flex-direction: column;
-}
-.demo-input-right {
-  width: 320px;
-  margin-bottom: 12px;
-  &.o-input {
-    --_box-padding: 0 15px 0 0;
-  }
-
-  :deep(.o_input-prefix) {
-    border-right: var(--_box-bd);
-    margin-right: 16px;
-  }
-}
-
-.demo-select {
-  --select-icon-gap: 4px;
-  --select-padding: 0 7px;
-  border: none;
-}
-
-.demo-input-left {
-  width: 320px;
-  &.o-input {
-    --_box-padding: 0 0 0 15px;
-  }
-
-  :deep(.o_input-suffix) {
-    border-left: var(--_box-bd);
-    margin-left: 16px;
-  }
-
   .demo-select {
-    width: 100px;
+    --select-icon-gap: 4px;
+    --select-padding: 0 7px;
+  }
+
+  .demo-wrap-top {
+    display: flex;
+    align-items: center;
+    margin-bottom: 12px;
+    .demo-input {
+      position: relative;
+      z-index: 1;
+      width: 200px;
+
+      :deep(.o_box-main) {
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+      }
+    }
+
+    .demo-select {
+      position: relative;
+      z-index: 0;
+      width: 120px;
+      margin-left: -1px;
+      border-top-left-radius: 0;
+      border-bottom-left-radius: 0;
+
+      &:hover {
+        z-index: 2;
+      }
+    }
+  }
+
+  .demo-wrap-bottom {
+    display: flex;
+    align-items: center;
+    .demo-input {
+      position: relative;
+      z-index: 1;
+      width: 220px;
+
+      :deep(.o_box-main) {
+        border-top-left-radius: 0;
+        border-bottom-left-radius: 0;
+      }
+    }
+
+    .demo-select {
+      z-index: 0;
+      width: 100px;
+      margin-right: -1px;
+      border-top-right-radius: 0;
+      border-bottom-right-radius: 0;
+
+      &:hover {
+        z-index: 2;
+      }
+    }
   }
 }
 </style>
