01ba28f0302a74872219f3f22e90845769b08879
AC-0308 <pzzxtyh@qq.com>
2026-03-24 14:44:22 +0800

    fix(OTab): 🐛 修复`--tab-nav-justify`变量未被正确应用的问题

---
+++ packages/opendesign/src/tab/__docs__/__case__/AlignLeft.vue
@@ -0,0 +1,25 @@
+<docs lang="md">
+<!-- zh-CN -->
+
+### 左对齐
+
+<!-- en-US -->
+
+### Align Left
+</docs>
+
+<script setup lang="ts"></script>
+
+<template>
+  <OTab class="my-tab">
+    <OTabPane label="Tab 1">Tab content 1</OTabPane>
+    <OTabPane label="Tab 2">Tab content 2</OTabPane>
+    <OTabPane label="Tab 3">Tab content 3</OTabPane>
+  </OTab>
+</template>
+
+<style scoped lang="scss">
+.my-tab {
+  --tab-nav-justify: flex-start;
+}
+</style>
