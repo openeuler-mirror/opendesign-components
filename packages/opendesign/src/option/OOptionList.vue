fab3a960c0bd8ae1fb66772d4aead8e286d2c76d
AC-0308 <pzzxtyh@qq.com>
2026-08-18 09:24:16 +0800

    fix(OOption): 🐛 使用 `OScrollbar` 替代 `v-scrollbar` 以提供响应性

--- packages/opendesign/src/option/OOptionList.vue
+++ packages/opendesign/src/option/OOptionList.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { vScrollbar, type BaseScrollerPropsT } from '../scrollbar';
+import { computed, ref } from 'vue';
+import { OScrollbar, type BaseScrollerPropsT } from '../scrollbar';
+
 interface OptionPropT {
   wrapClass?: string | { [k: string]: boolean } | Array<{ [k: string]: boolean } | string>;
-  // 是否使用scrollbar
+  /** 是否使用滚动条 */
   scrollbar?: boolean | Partial<BaseScrollerPropsT>;
 }
-const props = defineProps<OptionPropT>();
+
+const props = withDefaults(defineProps<OptionPropT>(), {
+  wrapClass: undefined,
+  scrollbar: true,
+});
+
+/** 滚动容器引用，供 OScrollbar 绑定 */
+const containerRef = ref<HTMLElement>();
 
 /**
- * 设置滚动条参数
+ * 滚动条参数，同时作为 OScrollbar 组件的渲染条件（v-if）与 props 数据源（v-bind）
+ * @description scrollbar=true 时提供默认配置；为 false 时返回 falsy 值以跳过 OScrollbar 渲染
  */
 const scrollbarProps = computed(() => {
   if (props.scrollbar === true) {
@@ -21,10 +30,12 @@ const scrollbarProps = computed(() => {
   return props.scrollbar;
 });
 </script>
+
 <template>
-  <div class="o-option-list">
-    <div v-scrollbar="scrollbarProps" class="o-options-container" :class="props.wrapClass">
+  <div class="o-option-list" :class="{ 'o-scrollbar-wrapper': scrollbarProps }">
+    <div ref="containerRef" class="o-options-container" :class="props.wrapClass">
       <slot></slot>
     </div>
+    <OScrollbar v-if="scrollbarProps" :target="containerRef" v-bind="scrollbarProps" />
   </div>
 </template>
